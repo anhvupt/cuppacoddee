6ae324c88fe1cdf6a25b11a963e5ae9ba7bb8357
Anhvupt <vu.pham@impartner.com>
2022-10-04 14:33:29 +0700

    feat: init scully blog

--- scully.cuppacoddee.config.ts
+++ scully.cuppacoddee.config.ts
@@ -1,13 +1,18 @@
 import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
-import '@scullyio/scully-plugin-puppeteer'
+import '@scullyio/scully-plugin-puppeteer';
 
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "cuppacoddee",
+  projectRoot: './src',
+  projectName: 'cuppacoddee',
   // add spsModulePath when using de Scully Platform Server,
   outDir: './dist/static',
   routes: {
-  }
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },}
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ScullyLibModule
+    ScullyLibModule,
+    RouterModule.forRoot([
+      { path: 'blog', loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule) }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
