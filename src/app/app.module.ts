1091bf98d826571c3af8f284f53b68335972ffee
Anhvupt <vu.pham@impartner.com>
2022-10-21 10:14:45 +0700

    add about page and set up to deploy to firebase with github action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutMeComponent],
   imports: [
     BrowserModule,
     ScullyLibModule,
     RouterModule.forRoot([
-      { path: 'blog', loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule) }
+      { path: '', redirectTo: 'blog', pathMatch: 'full' },
+      { path: 'blog', loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule) },
+      {
+        path: 'about',
+        component: AboutMeComponent
+      },
+      { path: '**', component: AboutMeComponent }
     ])
   ],
   providers: [],
